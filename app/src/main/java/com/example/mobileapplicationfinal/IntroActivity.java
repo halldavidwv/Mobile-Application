6498ea6438504e9a0f7a76fd7d58a4e13fdf426f
halldavidwv <hall.davidwv@gmail.com>
2024-12-04 15:17:45 +0800

    feat: add animation in splash screen
    Added animation in splash screen whenever opening the app.

--- app/src/main/java/com/example/mobileapplicationfinal/IntroActivity.java
+++ app/src/main/java/com/example/mobileapplicationfinal/IntroActivity.java
@@ -10,34 +10,35 @@ import androidx.core.view.WindowInsetsCompat;
 import android.content.Intent;
 import android.annotation.SuppressLint;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
-
-import com.example.mobileapplicationfinal.ui.login.LoginActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class IntroActivity extends AppCompatActivity {
-    private Button proceedButton;
 
-    @SuppressLint("MissingInflatedId")
+    Animation top_animation, bottom_animation;
+    ImageView image;
+    TextView logo, slogan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_intro);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
 
-        proceedButton = findViewById(R.id.proceed_button);
+        top_animation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottom_animation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
 
-        proceedButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent loginIntent = new Intent(IntroActivity.this, LoginActivity.class);
-                startActivity(loginIntent);
-            }
-        });
+        image = findViewById(R.id.main_logo_image_view);
+        logo = findViewById(R.id.intro_activity_title);
+        slogan = findViewById(R.id.intro_activity_description);
+
+        image.setAnimation(top_animation);
+        logo.setAnimation(bottom_animation);
+        slogan.setAnimation(bottom_animation);
 
     }
 }
--- app/src/main/java/com/example/mobileapplicationfinal/ui/forum/ForumFragment.java
+++ app/src/main/java/com/example/mobileapplicationfinal/ui/forum/ForumFragment.java
@@ -10,22 +10,22 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.mobileapplicationfinal.databinding.FragmentNotificationsBinding;
+import com.example.mobileapplicationfinal.databinding.FragmentForumBinding;
 
 public class ForumFragment extends Fragment {
 
-    private FragmentNotificationsBinding binding;
+    private FragmentForumBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        com.example.mobileapplicationfinal.ui.forum.MilestonesViewModel milestonesViewModel =
-                new ViewModelProvider(this).get(com.example.mobileapplicationfinal.ui.forum.MilestonesViewModel.class);
+        com.example.mobileapplicationfinal.ui.forum.ForumViewModel forumViewModel =
+                new ViewModelProvider(this).get(com.example.mobileapplicationfinal.ui.forum.ForumViewModel.class);
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
+        binding = FragmentForumBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textNotifications;
-        milestonesViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.forumNameTextView;
+        forumViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
